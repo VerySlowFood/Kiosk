675891aa1e7c1059dfbb7c6264f7bd2f919d6537
bedro05 <goodwon48337@naver.com>
2021-10-27 16:32:26 +0900

    UI 작업

--- app/src/main/java/com/slowfood/kiosk/MainActivity.java
+++ app/src/main/java/com/slowfood/kiosk/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private Button button;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
